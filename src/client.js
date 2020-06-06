ce902377b4ee3d3d4a7fc512a777a1267b94fe09
Skayo <jonas@skayo.dev>
2020-06-06 02:16:15 +0200

    Added analytics

--- src/client.js
+++ src/client.js
@@ -1,8 +1,14 @@
 import * as sapper from '@sapper/app';
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import ackeeTracker from 'ackee-tracker'
 
 UIkit.use(Icons);
+
+ackeeTracker.create({
+	server: 'https://analytics.skayo.dev',
+	domainId: '4a712c06-1eee-4202-bcc1-f7bf0edf4d83'
+}).record()
 
 sapper.start({
 	target: document.querySelector('#sapper')
