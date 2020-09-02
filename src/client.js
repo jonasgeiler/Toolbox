fa286724462f03adbdc47de0a5f8eefc09810971
Skayo <jonas@skayo.dev>
2020-09-02 09:52:53 +0200

    Removed ackee tracker from client.js
    Adding it to html directly...

--- src/client.js
+++ src/client.js
@@ -1,14 +1,8 @@
 import * as sapper from '@sapper/app';
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
-import ackeeTracker from 'ackee-tracker'
 
 UIkit.use(Icons);
-
-ackeeTracker.create({
-	server: 'https://analytics.skayo.dev',
-	domainId: 'b9234509-bacd-4623-b4db-af3f12fee5da'
-}).record()
 
 sapper.start({
 	target: document.querySelector('#sapper')
